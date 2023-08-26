524320bdf98aa4bc6f367c0ca9da5cda9548f53f
sudheer chowdari kodali <45354847+sudheerkodali@users.noreply.github.com>
2023-08-26 15:44:30 +0530

    Add files via upload

---
+++ DSA-1/index.js
@@ -0,0 +1,96 @@
+//Array is linear data structure that stores a collection of elements of the same type//
+// Array are stored in contiguous memeory locations //
+
+//let numbers = [1, 2, 3, 4, 5];
+//console.log(numbers[0]); // Output: 1
+//console.log(numbers[2]); // Output: 3
+
+
+//linked list//
+//A linked list is a linear data structure where elemnts are stored in nodes that are connected thorugh pointers//
+//Each node reference pointer and next node in the list//
+
+//class Node {
+//  constructor(data) {
+//    this.data = data;
+//  this.next = null;
+//  }
+//}
+
+//let list = new Node(1);
+//list.next = new Node(2);
+//list.next.next = new Node(3);
+//console.log(list.data); // Output: 1
+//console.log(list.next.data); // Output: 2
+
+// Stacks
+//A stack is a linear data structure that follows the Last-In-First-Out (LIFO) principle.//
+
+//let stack = []
+//stack.push(1);
+//stack.push(2);
+//stack.push(3);
+//console.log(stack.pop()); //output: 3
+//console.log(stack.pop()); //output: 2
+
+// queues
+//A queue is a linear data structure that follows the First-In-First-Out (FIFO) principle.//
+
+//let queue = [];
+//queue.push(1);
+//queue.push(2);
+
+//queue.push(3);
+//console.log(queue.shift()); //output:1
+//console.log(queue.shift()); //output:2
+
+
+
+//function demonstrateLinearDataTypes() {
+    // Array
+    //var arr = [1, 2, 3, 4];
+    //console.log("Array: " + arr);
+
+    // Linked List
+    //class Node {
+        //constructor(data) {
+            //this.data = data;
+            //
+            //this.next = null;
+     //   }
+
+
+
+
+   // }
+   // var list = new Node(1);
+   // list.next = new Node(2);
+    //list.next.next = new Node(3);
+   // console.log("Linked List:");
+   // console.log(list);
+
+    // Stack
+    //var stack = [];
+    //stack.push(1);
+    //stack.push(2);
+   // stack.push(3);
+   // console.log("Stack:");
+   // console.log(stack);
+
+    // Queue
+    //var queue = [];
+    //queue.push(1);
+    //queue.push(2);
+    //queue.push(3);
+    //console.log("Queue:");
+    //console.log(queue);
+//}
+
+// Call the function when the page is loaded
+//window.addEventListener('load', function () {
+  //  demonstrateLinearDataTypes();
+//});
+
+
+
+
